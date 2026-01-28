a55bd8d11ace3a697232dd0088367b13ad5da44b
Jessica <ishimwejessica2@gmail.com>
2026-01-28 09:16:26 +0200

    first commit

---
+++ first-react/src/App.jsx
@@ -0,0 +1,10 @@
+import Main from "./component/Main.jsx"
+import Navbar from "./component/Navbar.jsx"
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
---
+++ first-react/src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+export default function Navbar() {
+    return (
+        <header >
+            <nav>
+                <img className="logo" src="./src/assets/react.svg" alt="" />
+                <span className="logoname">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
